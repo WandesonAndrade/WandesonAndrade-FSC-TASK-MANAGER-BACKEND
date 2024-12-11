526f428a4494813240435851ae858e2167f669bc
WandesonAndrade <wandesonandrade33@gmail.com>
2024-12-11 10:47:01 -0300

    feat: add model

---
+++ src/models/task.models.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require("mongoose");
+
+const TaskSkema = Schema({
+    description: {
+        type: String,
+        required: true,
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const taskModel = model("Task", TaskSkema);
